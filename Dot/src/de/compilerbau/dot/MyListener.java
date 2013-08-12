f2acfcacf5956263b323bf11d141b8ddad1d29d8
DennisBlock <dennis.block@gmx.de>
2013-08-12 16:50:13 +0200

    build xml

--- Dot/src/de/compilerbau/dot/MyListener.java
+++ Dot/src/de/compilerbau/dot/MyListener.java
@@ -4,37 +4,45 @@
 package de.compilerbau.dot;
 
 import de.compilerbau.dot.DOTParser.FileContext;
+import de.compilerbau.dot.DOTParser.GraphContext;
 import de.compilerbau.dot.DOTParser.OnlyContext;
 import de.compilerbau.dot.DOTParser.UncoverContext;
 
 /**
  * TODO Add comment here
- *
- * @author $Author:  $
- * @version $Revision:  $, $Date:  $ UTC
+ * 
+ * @author $Author: $
+ * @version $Revision: $, $Date: $ UTC
  */
 public class MyListener extends DOTBaseListener
 {
 
-   @Override
-   public void enterUncover(UncoverContext ctx)
-   {
-//       System.out.println(ctx. == DOTParser.GRAPH ? "asd" : "asd");
-      super.enterUncover(ctx);
-   }
+    @Override
+    public void enterUncover(UncoverContext ctx)
+    {
+	// System.out.println(ctx. == DOTParser.GRAPH ? "asd" : "asd");
+	super.enterUncover(ctx);
+    }
 
-   @Override
-   public void enterOnly(OnlyContext ctx)
-   {
-      // TODO Auto-generated method stub
-      super.enterOnly(ctx);
-   } 
- 
-   @Override
-   public void enterFile(FileContext ctx)
-   {
-       System.out.println(ctx.getText());
-      super.enterFile(ctx);
-   }
+    @Override
+    public void enterOnly(OnlyContext ctx)
+    {
+	// TODO Auto-generated method stub
+	super.enterOnly(ctx);
+    }
+
+    @Override
+    public void enterFile(FileContext ctx)
+    {
+	System.out.println(ctx.getText());
+	super.enterFile(ctx);
+    }
+
+    @Override
+    public void enterGraph(GraphContext ctx)
+    {
+	super.enterGraph(ctx);
+	System.out.println(ctx.getText());
+    }
 
 }
