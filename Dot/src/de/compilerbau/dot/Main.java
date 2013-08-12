e87edee31c5cf3185eee7ae3e15c4a3674009f5d
DennisBlock <dennis.block@gmx.de>
2013-08-12 17:03:40 +0200

    qwd

--- Dot/src/de/compilerbau/dot/Main.java
+++ Dot/src/de/compilerbau/dot/Main.java
@@ -1,61 +1,84 @@
-/**
- * 
- */
 package de.compilerbau.dot;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import att.grappa.Graph;
+import att.grappa.Grappa;
+import att.grappa.GrappaAdapter;
+import att.grappa.GrappaConstants;
+import att.grappa.GrappaPanel;
+import att.grappa.GrappaSupport;
+import att.grappa.Parser;
+
 /**
  * TODO Add comment here
- *
- * @author $Author:  $
- * @version $Revision:  $, $Date:  $ UTC
+ * 
+ * @author $Author: $
+ * @version $Revision: $, $Date: $ UTC 
  */
-public class Main
+public class Main implements GrappaConstants 
 {
 
    /**
     * TODO Add method comment here
-    *
+    * 
     * @param args
     */
    public static void main(String[] args)
    {
       try
       {
-         String inputFile = "src\\de\\compilerbau\\dot\\t.expr"; 
-         InputStream is; 
+         String inputFile = "src\\de\\compilerbau\\dot\\t.expr";
+         InputStream is;
          is = new FileInputStream(inputFile);
-         ANTLRInputStream input = new ANTLRInputStream(is); 
+         ANTLRInputStream input = new ANTLRInputStream(is);
          DOTLexer lexer = new DOTLexer(input);
          CommonTokenStream tokens = new CommonTokenStream(lexer);
-         DOTParser parser = new DOTParser(tokens);   
+         DOTParser parser = new DOTParser(tokens);
          ParseTree t = parser.s();
-         
+
          // System.out.println(t.toStringTree(parser));
          ParseTreeWalker walker = new ParseTreeWalker();
          MyListener listen = new MyListener();
-         walker.walk(listen, t);  
-//      System.out.println("Ergebnis: "+listen.returnValue()); 
+         walker.walk(listen, t);
+         
+         
+         // System.out.println("Ergebnis: "+listen.returnValue());
+         InputStream input2 = null;
+//         new Demo12().doDemo(input2);
       }
-      catch (FileNotFoundException e) 
+      catch (FileNotFoundException e)
       {
          // TODO Auto-generated catch block
          e.printStackTrace();
       }
-      catch (IOException e) 
+      catch (IOException e)
       {
          // TODO Auto-generated catch block
          e.printStackTrace();
       }
-   } 
-
+   }
 }
--- Dot/src/de/compilerbau/dot/MyListener.java
+++ Dot/src/de/compilerbau/dot/MyListener.java
@@ -4,7 +4,6 @@
 package de.compilerbau.dot;
 
 import de.compilerbau.dot.DOTParser.FileContext;
-import de.compilerbau.dot.DOTParser.GraphContext;
 import de.compilerbau.dot.DOTParser.OnlyContext;
 import de.compilerbau.dot.DOTParser.UncoverContext;
 
@@ -17,32 +16,30 @@ import de.compilerbau.dot.DOTParser.UncoverContext;
 public class MyListener extends DOTBaseListener
 {
 
-    @Override
-    public void enterUncover(UncoverContext ctx)
-    {
-	// System.out.println(ctx. == DOTParser.GRAPH ? "asd" : "asd");
-	super.enterUncover(ctx);
-    }
+   public String returnValue(String value)
+   {
+      return value;
+   }
 
-    @Override
-    public void enterOnly(OnlyContext ctx)
-    {
-	// TODO Auto-generated method stub
-	super.enterOnly(ctx);
-    }
+   @Override
+   public void enterUncover(UncoverContext ctx)
+   {
+      super.enterUncover(ctx);
+   }
 
-    @Override
-    public void enterFile(FileContext ctx)
-    {
-	System.out.println(ctx.getText());
-	super.enterFile(ctx);
-    }
+   @Override
+   public void enterOnly(OnlyContext ctx)
+   {
+      // TODO Auto-generated method stub
+      super.enterOnly(ctx);
+   }
 
-    @Override
-    public void enterGraph(GraphContext ctx)
-    {
-	super.enterGraph(ctx);
-	System.out.println(ctx.getText());
-    }
+   @Override
+   public void enterFile(FileContext ctx)
+   {
+      System.out.println(ctx.children.get(0).getText());
+      System.out.println(ctx.getText());
+      super.enterFile(ctx);
+   }
 
 }
